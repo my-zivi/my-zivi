f7d6f0fb4724ed16bd87c85889a7c36be26115de
Simon Rösch <roesch@ubique.ch>
2017-08-02 14:28:19 +0200

    pflichtenheft edit & add

--- api/app/Specification.php
+++ api/app/Specification.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Specification extends Model
 {
+    public $timestamps = false;
+
     protected $fillable = ['id',
                            'name ',
                            'short_name ',
