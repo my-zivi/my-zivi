e59f1b41b3ad4a06f900a9c95af632de77d0f79a
John Lin <john@lin.to>
2017-08-09 14:08:43 +0200

    fixed warning about button trying to submit (prevent default behaviour)

--- web-client/src/views/pages/profile.js
+++ web-client/src/views/pages/profile.js
@@ -486,7 +486,7 @@ export default class User extends Component {
   getPasswordChangeButton() {
     return (
       <div>
-        <button name="resetPassword" class="btn btn-primary" onClick={e => this.redirectToChangePassword(e)}>
+        <button type="button" name="resetPassword" class="btn btn-primary" onClick={e => this.redirectToChangePassword(e)}>
           Passwort ändern
         </button>
         <hr />
