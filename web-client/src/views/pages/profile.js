030ae95a8940a823853768bda6af94987ec2f928
Clot Schaniel <clot@schaniel.net>
2017-08-10 16:11:25 +0200

    replace col-1 to col-2 for minor styling

--- web-client/src/views/pages/profile.js
+++ web-client/src/views/pages/profile.js
@@ -398,7 +398,7 @@ export default class User extends Component {
                 <div class="row">
                   <div class="col-xs-2">Von</div>
                   <div class="col-xs-2">Bis</div>
-                  <div class="col-xs-1">Angerechnete Tage</div>
+                  <div class="col-xs-2">Angerechnete Tage</div>
                   {ApiService.isAdmin() ? <div class="col-xs-1">Erledigt</div> : null}
                   <div class="col-xs-2" />
                   {ApiService.isAdmin() ? <div class="col-xs-2" /> : null}
@@ -410,7 +410,7 @@ export default class User extends Component {
                       <div class="row">
                         <div class="col-xs-2">{moment(obj.start, 'YYYY-MM-DD').format('DD.MM.YYYY')}</div>
                         <div class="col-xs-2">{moment(obj.end, 'YYYY-MM-DD').format('DD.MM.YYYY')}</div>
-                        <div class="col-xs-1">{obj.days}</div>
+                        <div class="col-xs-2">{obj.days}</div>
                         {ApiService.isAdmin() ? obj.done === 1 ? <div class="col-xs-1">&#9989;</div> : <div class="col-xs-1" /> : null}
                         <div class="col-xs-2">
                           <button name="showReportSheet" class="btn btn-xs" onClick={() => this.showReportSheet(obj.id)}>
