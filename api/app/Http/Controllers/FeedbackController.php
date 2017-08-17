0652b5582553ce9beb257eedcaafd93ea1e5326d
Michael Schaer [Notebook] <mdschaer@gmail.com>
2017-08-17 08:47:27 +0200

    FeedbackController works for up to 6 answers

--- api/app/Http/Controllers/FeedbackController.php
+++ api/app/Http/Controllers/FeedbackController.php
@@ -16,25 +16,20 @@ class FeedbackController extends Controller
     public function getFeedbacks()
     {
         $this->output = new ConsoleOutput();
-
         $questions = DB::table('user_feedback_questions')->get();
 
         for ($i = 0; $i < count($questions); $i++) {
-            //$this->output->writeln("questionID = ".$question->id);
-
             switch ($questions[$i]->type) {
                 case 0:
                     $questions[$i]->answers = null;
                     break;
                 case 1:
-                    $questions[$i]->answers = $this->getFeedbacksType1($questions[$i]->id);
+                    $questions[$i]->answers = $this->getFeedbacksTypeRange($questions[$i]->id);
                     break;
                 case 2:
-                    $questions[$i]->answers = $this->getFeedbacksType2($questions[$i]->id);
+                    $questions[$i]->answers = $this->getFeedbacksTypeText($questions[$i]->id);
                     break;
             }
-
-            //$this->output->writeln("Answers = ".json_encode($question['answers']));
         }
 
         return new JsonResponse($questions);
@@ -47,32 +42,27 @@ class FeedbackController extends Controller
      (SELECT COUNT(*) FROM user_feedback WHERE answer = 3 AND questionId = 3) AS '3',
      (SELECT COUNT(*) FROM user_feedback WHERE answer = 4 AND questionId = 3) AS '4';
     */
-    private function getFeedbacksType1($questionId)
+    private function getFeedbacksTypeRange($questionId)
     {
-
-        // works:
-        //$results = DB::table('user_feedback')->where('answer', '=', 1)->get()->count();
-        //DB::table('user_feedback')->select(DB::raw('COUNT(*) AS "1"'))->where('answer', '=', 1)->where('questionId', '=', $questionId)->get();
-
         $results = array();
-        $results[1] = DB::table('user_feedback')->where('answer', '=', 1)->where('questionId', '=', $questionId)->get()->count();
-        $results[2] = DB::table('user_feedback')->where('answer', '=', 2)->where('questionId', '=', $questionId)->get()->count();
-        $results[3] = DB::table('user_feedback')->where('answer', '=', 3)->where('questionId', '=', $questionId)->get()->count();
-        $results[4] = DB::table('user_feedback')->where('answer', '=', 4)->where('questionId', '=', $questionId)->get()->count();
+
+        for ($i = 0; $i <= 5; $i++) {
+            $results[$i] = DB::table('user_feedback')->where('answer', '=', $i)->where('questionId', '=', $questionId)->get()->count();
+        }
 
         return $results;
     }
 
-    private function getFeedbacksType2($questionId)
+    private function getFeedbacksTypeText($questionId)
     {
         $results = DB::table('user_feedback')->select('answer')->where('questionId', '=', $questionId)->get();
 
-        $returnObj = "";
-        foreach ($results as $result) {
-            //$this->output->writeln(json_encode($result->answer));
-            $returnObj .= "\n\r | \n\r " . $result->answer;
+        $answerTexts = "";
+        foreach ($results as $key => $value) {
+            //$this->output->writeln(json_encode($value->answer));
+            $answerTexts .= $value->answer . "\n\r";
         }
 
-        return $returnObj;
+        return $answerTexts;
     }
 }
