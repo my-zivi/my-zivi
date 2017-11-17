26a6b08d9bfeec8a82fc70ac080fa5b5f6b03287
jgschwend <gschwendlj@hotmail.com>
2017-11-17 13:46:35 +0100

    try to run yarn

--- CI/frontend.sh
+++ CI/frontend.sh
@@ -5,5 +5,6 @@ nodepath=`pwd`/node-v8.9.1-linux-x64/bin
 wget -q "https://nodejs.org/dist/v8.9.1/node-v8.9.1-linux-x64.tar.xz" && \
 tar xf node-v8.9.1-linux-x64.tar.xz && \
 cd web-client && \
-$nodepath/npm install -g yarn --verbose
-$nodepath/npm install 
+$nodepath/npm install -g yarn --verbose && \
+$nodepath/npm install && \
+yarn run watch
