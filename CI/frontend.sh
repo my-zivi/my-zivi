27297497138c6f702a6120b7b31ad4b40df772bb
jgschwend <gschwendlj@hotmail.com>
2017-11-17 13:34:06 +0100

    Download Nodejs during Frontend build and install yarn and packages

--- CI/frontend.sh
+++ CI/frontend.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -x
 
-ls -la
+nodepath=`pwd`/node-v8.9.1-linux-x64/bin
+
+wget - q https://nodejs.org/dist/v8.9.1/node-v8.9.1-linux-x64.tar.xz && \
+tar xf node-v8.9.1-linux-x64.tar.xz && \
+cd web-client && \
+$nodepath/npm install -g yarn --verbose
+$nodepath/npm install 
