7c6a9fc6992a3b42ada77b5e0a194d04495bf8ff
jgschwend <gschwendlj@hotmail.com>
2017-11-17 14:58:49 +0100

    Created deployment script

---
+++ CI/deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ ! -f $HOME/.ssh/id_rsa ]; then
+  echo "Setting up SSH key"
+  ./CI/establish-ssh.sh
+fi
+
+case $1 in
+  "test")
+    ENVIRONMENT=test
+    ;;
+  "prod")
+    ENVIRONMENT=prod
+    ;;
+  *)
+    echo "Invalid environment"
+    exit 1
+    ;;
+esac
+
+#Read config env
+$CONFIG_FILE=izivi.$ENVIRONMENT.env
+export $(ssh $TARGET "cat $CONFIG_FILE" | xargs)
+
+TMP=izivi_deploy_tmp
+BACKUP_DIR=$HOME/backup
+
+if [ -z "$PROJECT_DIR" ]; then
+  echo "PROJECT_DIR is not set"
+  exit 1
+fi
+
+
+ssh $TARGET mkdir -p ${PROJECT_DIR} ${PROJECT_DIR}.bak && \
+rsync -ra --exclude '.git' --exclude 'node_modules' . $TARGET:$TMP && \
+ssh $TARGET rm -r ${PROJECT_DIR}.bak && \
+ssh $TARGET mv $PROJECT_DIR ${PROJECT_DIR}.bak && \
+ssh $TARGET mv $TMP $PROJECT_DIR
+
+ssh $TARGET cp $CONFIG_FILE $PROJECT_DIR/api/.env
+
+ssh $TARGET mkdir -p $BACKUP_DIR
+ssh $TARGET "mysqldump -u $DATABASE_USER -p\"$DATABASE_PW\" $DATABASE | bzip2 -c > $BACKUP_DIR/$DB_DATABASE_$(date +%Y_%m_%d-%H:%M:%S).sql.bz2"
+ssh $TARGET "cd $PROJECT_DIR/api && php71 artisan migrate"
---
+++ CI/establish-ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+SSH_FILE=$HOME/.ssh/id_rsa
+openssl aes-256-cbc \
+    -K $encrypted_43243e92edfc_key \
+    -iv $encrypted_43243e92edfc_iv \
+    -in ".id_rsa.enc" \
+    -out "$SSH_FILE" -d
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Enable SSH authentication
+chmod 600 "$SSH_FILE"
+echo "stiftungswo.ch,149.126.4.24 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBN43sGVazZlEbitcxVESOns6JtFcXbrbu5/9AvBwacZNvhKofbiY2wn748QbrVAeAVRdfJ7W3/0t9sOLv/avvXs=" > $HOME/.ssh/known_hosts
