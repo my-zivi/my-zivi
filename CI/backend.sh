d2633689a1abd8a0124985affd65021a5b613aad
jgschwend <gschwendlj@hotmail.com>
2017-11-17 12:04:27 +0100

    Set working directory of composer

--- CI/backend.sh
+++ CI/backend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
 cp api/.env.travis api/.env && \
-composer install -n && \
+composer install --no-interaction --working-dir /api  && \
 php artisan db:create && \
 php artisan migrate
