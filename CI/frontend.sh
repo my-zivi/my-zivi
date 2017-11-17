71684c713154a1d7f48a1dec2171088d80c5d98a
jgschwend <gschwendlj@hotmail.com>
2017-11-17 13:38:16 +0100

    Fix download issues

--- CI/frontend.sh
+++ CI/frontend.sh
@@ -2,7 +2,7 @@
 
 nodepath=`pwd`/node-v8.9.1-linux-x64/bin
 
-wget - q https://nodejs.org/dist/v8.9.1/node-v8.9.1-linux-x64.tar.xz && \
+wget -q "https://nodejs.org/dist/v8.9.1/node-v8.9.1-linux-x64.tar.xz" && \
 tar xf node-v8.9.1-linux-x64.tar.xz && \
 cd web-client && \
 $nodepath/npm install -g yarn --verbose
