e78ae08bb9e0fecc83de0eaa8ab3b5f1a7637a34
jgschwend <gschwendlj@hotmail.com>
2017-11-23 18:22:38 +0100

    Remove Test logging

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -27,7 +27,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        \Log::info("Application started");
         $schedule->command(SendFeedbackRemainderMails::class)->daily();
     }
 }
