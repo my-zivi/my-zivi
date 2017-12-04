76f6266928cba3612992ea102075fd1b2d9c4146
tourn <latzer.daniel@gmail.com>
2017-12-04 13:44:27 +0100

    Update deployment script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f $HOME/.ssh/id_rsa ]; then
   echo "Setting up SSH key"
-  ./CI/establish-ssh.sh
+  ./ci/establish-ssh.sh
 fi
 
 case $1 in
@@ -18,10 +18,8 @@ case $1 in
     ;;
 esac
 
-cp web-client/htaccess.dist web-client/dist/.htaccess
-
 #Read config env
-CONFIG_FILE=izivi.$ENVIRONMENT.env
+CONFIG_FILE=deploy/izivi.$ENVIRONMENT.env
 TARGET=$target
 export $(ssh $TARGET "cat $CONFIG_FILE" | xargs)
 
@@ -33,15 +31,16 @@ if [ -z "$PROJECT_DIR" ]; then
   exit 1
 fi
 
-
+# upload and move folders
+cp web-client/htaccess.dist web-client/dist/.htaccess && \
 ssh $TARGET mkdir -p ${PROJECT_DIR} ${PROJECT_DIR}.bak && \
 rsync -ra --exclude '.git' --exclude 'node_modules' . $TARGET:$TMP && \
+ssh $TARGET cp $CONFIG_FILE $TMP/api/.env && \
 ssh $TARGET rm -r ${PROJECT_DIR}.bak && \
 ssh $TARGET mv $PROJECT_DIR ${PROJECT_DIR}.bak && \
 ssh $TARGET mv $TMP $PROJECT_DIR
 
-ssh $TARGET cp $CONFIG_FILE $PROJECT_DIR/api/.env
-
-ssh $TARGET mkdir -p $BACKUP_DIR
-ssh $TARGET "mysqldump -u $DB_USERNAME -p$DB_PASSWORD $DB_DATABASE | bzip2 -c > $BACKUP_DIR/${DB_DATABASE}_$(date +%Y_%m_%d-%H:%M:%S).sql.bz2"
-ssh $TARGET "cd $PROJECT_DIR/api && php71 artisan migrate"
+# db backup and migrate
+ssh $TARGET mkdir -p $BACKUP_DIR && \
+ssh $TARGET "mysqldump -u $DB_USERNAME -p$DB_PASSWORD $DB_DATABASE | bzip2 -c > $BACKUP_DIR/${DB_DATABASE}_$(date +%Y_%m_%d-%H:%M:%S).sql.bz2" && \
+ssh $TARGET "cd $PROJECT_DIR/api && php72 artisan migrate"
