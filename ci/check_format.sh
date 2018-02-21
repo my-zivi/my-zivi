d691fbdf2fa29dc8b15ee2c56bcc3c75b4294fdb
Paul Sonnenschein <paulsonnenschein0@gmail.com>
2018-02-21 16:33:18 +0100

    Add comments [ci skip]

--- ci/check_format.sh
+++ ci/check_format.sh
@@ -1,5 +1,10 @@
-#api
+# checks if formatters could have been run locally to fix the code style 
+
+# check backend (api)
 composer global require "squizlabs/php_codesniffer=*"
+# we run phpcbf twice to check if it was able to fix something
+# since it reports more than it can fix we check the status code twice and check if it changed
+# sadly there is no unique status code for "phpcbf fixed something and maybe there are unfixable errors"
 $HOME/.composer/vendor/bin/phpcbf --standard=psr2 api/
 code1=$?
 $HOME/.composer/vendor/bin/phpcbf --standard=psr2 api/ > /dev/null 2>&1
@@ -9,7 +14,7 @@ if [ $code1 -ne $code2 ]; then
   exit 1;
 fi
 
-#frontend
+# check frontend (web-client)
 npm install -g prettier
 cd web-client
 prettier --list-different --print-width 140 --single-quote --trailing-comma es5 "src/**/*.js"
