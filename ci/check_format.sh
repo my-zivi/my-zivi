cf363ddc3d6c27ceffd4ccf0263cf509a9064f36
tourn <latzer.daniel@gmail.com>
2018-02-26 09:15:06 +0100

    Consequently use Yarn
    See #89

--- ci/check_format.sh
+++ ci/check_format.sh
@@ -15,9 +15,8 @@ if [ $code1 -ne $code2 ]; then
 fi
 
 # check frontend (web-client)
-npm install -g prettier
 cd web-client
-prettier --list-different --print-width 140 --single-quote --trailing-comma es5 "src/**/*.js"
+npx prettier --list-different --print-width 140 --single-quote --trailing-comma es5 "src/**/*.js"
 if [ $? -ne 0 ]; then
   echo "Web-client is not properly formatted. Please reformat and commit again. See https://github.com/stiftungswo/izivi/tree/master/web-client#formatting"
   exit 1;
--- ci/frontend.sh
+++ ci/frontend.sh
@@ -5,5 +5,5 @@ nvm install 8.3 && \
 nvm use 8.3 && \
 cd web-client && \
 npm install -g yarn --verbose && \
-npm install && \
+yarn install && \
 yarn build
