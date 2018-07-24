24950e17977bf1c35a5f7022c62929b90e15f378
plumped <41291962+plumped@users.noreply.github.com>
2018-07-24 11:59:19 +0200

    Format

--- web-client/src/views/pages/home.js
+++ web-client/src/views/pages/home.js
@@ -1,29 +1,30 @@
 import Card from '../tags/card';
 import Header from '../tags/header';
 
-export default function (props) {
+export default function(props) {
   return (
     <Header>
       <div className="page page__home">
-        <div class="bg"><br />
-        <Card>
-          <h1>
-            iZivi <span> ist ein Tool der SWO zur Erfassung und Planung von Zivi-Einsätzen</span>
-          </h1>
+        <div class="bg">
           <br />
-          <p>
-            Bist du das erste mal bei uns und möchtest einen Einsatz planen? Dann kannst du dich über folgenden Link{' '}
-            <a href="//izivi.stiftungswo.ch/register" target="_self">
-              Registrieren
-            </a>.
-          </p>
-          <p>
-            Falls du uns bereits bekannt bist, kannst du dich hier{' '}
-            <a href="//izivi.stiftungswo.ch/login" target="_self">
-              Anmelden
-            </a>.
-          </p>
-        </Card>
+          <Card>
+            <h1>
+              iZivi <span> ist ein Tool der SWO zur Erfassung und Planung von Zivi-Einsätzen</span>
+            </h1>
+            <br />
+            <p>
+              Bist du das erste mal bei uns und möchtest einen Einsatz planen? Dann kannst du dich über folgenden Link{' '}
+              <a href="//izivi.stiftungswo.ch/register" target="_self">
+                Registrieren
+              </a>.
+            </p>
+            <p>
+              Falls du uns bereits bekannt bist, kannst du dich hier{' '}
+              <a href="//izivi.stiftungswo.ch/login" target="_self">
+                Anmelden
+              </a>.
+            </p>
+          </Card>
         </div>
       </div>
     </Header>
