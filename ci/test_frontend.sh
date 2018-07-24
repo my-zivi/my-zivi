f8778086f4f1689d553cba3a3c8339ec1897f40d
Andy Pfister <andy.pfister@hotmail.ch>
2018-07-24 15:07:44 +0200

    do not create video on ci run

--- ci/test_frontend.sh
+++ ci/test_frontend.sh
@@ -2,5 +2,5 @@
 
 if [[ $TRAVIS_COMMIT_MESSAGE != *"[skip-tests]"* ]]; then
     php api/artisan migrate:fresh --seed -q
-    cd web-client && CYPRESS_BASE_URL=http://localhost:3000 yarn run cypress run
+    cd web-client && yarn run cypress run --config video=false,baseUrl="http://localhost:3000"
 fi
