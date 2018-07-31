4371e58d530f6cdd9f1841aa5be175187cb75be4
Andy Pfister <andy.pfister@hotmail.ch>
2018-07-31 13:02:38 +0200

    use new build stages of travis (#113)

--- ci/test_backend.sh
+++ ci/test_backend.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -vue
 
 if [[ $TRAVIS_COMMIT_MESSAGE != *"[skip-tests]"* ]]; then
+    cp ./api/.env.travis ./api/.env
+    php api/artisan db:create
     php api/artisan migrate:fresh -q
     ./api/vendor/phpunit/phpunit/phpunit -c api --coverage-clover=coverage.xml
+    if [ $? == 0 ]; then
+        bash <(curl -s https://codecov.io/bash)
+    fi
 fi
