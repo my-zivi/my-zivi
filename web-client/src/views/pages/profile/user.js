db77aed0fa4c0a6294b6ee3235e6171cd1ec91bc
Jonathan Stahl <joni@stahlfamily.ch>
2018-08-29 14:40:31 +0200

    fix format

--- web-client/src/views/pages/profile/user.js
+++ web-client/src/views/pages/profile/user.js
@@ -145,11 +145,7 @@ export default class User extends Component {
           <InputField id={'zdp'} label={'ZDP'} value={user.zdp} disabled={true} />
           <InputField id={'first_name'} label={'Vorname'} value={user.first_name} onInput={this.handleChange} />
           <InputField id={'last_name'} label={'Nachname'} value={user.last_name} onInput={this.handleChange} />
-            { 'Murloc'.localeCompare(user.last_name, 'en', {sensitivity: 'base'}) === 0 &&
-              <EasterEgg
-                source={'/audio/murloc.mp3'}
-                />
-            }
+          {'Murloc'.localeCompare(user.last_name, 'en', { sensitivity: 'base' }) === 0 && <EasterEgg source={'/audio/murloc.mp3'} />}
 
           <InputField id={'address'} label={'Strasse'} value={user.address} onInput={this.handleChange} />
           <InputField id={'zip'} label={'PLZ'} value={user.zip} onInput={this.handleChange} />
@@ -327,8 +323,4 @@ export default class User extends Component {
   }
 }
 
-const EasterEgg = ({ source }) =>
-    <audio
-        autoPlay
-        src={source}>
-    </audio>
+const EasterEgg = ({ source }) => <audio autoPlay src={source} />;
