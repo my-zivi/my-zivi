0dc2414ae7ce01e3b5c702640541b93590e2deb2
tourn <latzer.daniel@gmail.com>
2019-01-04 15:47:21 +0100

    Extract navigation Component

---
+++ frontend/src/layout/Navigation.tsx
@@ -0,0 +1,77 @@
+import NavItem from 'reactstrap/lib/NavItem';
+import { Link, Route } from 'react-router-dom';
+import * as React from 'react';
+import { ReactNode } from 'react';
+import { inject, observer } from 'mobx-react';
+import Navbar from 'reactstrap/lib/Navbar';
+import NavbarBrand from 'reactstrap/lib/NavbarBrand';
+import NavbarToggler from 'reactstrap/lib/NavbarToggler';
+import Collapse from 'reactstrap/lib/Collapse';
+import Nav from 'reactstrap/lib/Nav';
+import NavLink from 'reactstrap/lib/NavLink';
+import { MainStore } from '../stores/mainStore';
+import { ApiStore } from '../stores/apiStore';
+
+interface NavEntryProps {
+  to: string;
+  children: ReactNode;
+  exact?: boolean;
+}
+
+const NavEntry = ({ to, children, exact }: NavEntryProps) => (
+  <Route
+    path={to}
+    exact={exact}
+    children={({ match }) => (
+      <NavItem active={Boolean(match)}>
+        <Link className="nav-link" to={to}>
+          {children}
+        </Link>
+      </NavItem>
+    )}
+  />
+);
+
+interface NavProps {
+  mainStore?: MainStore;
+  apiStore?: ApiStore;
+}
+
+@inject('mainStore', 'apiStore')
+@observer
+export class Navigation extends React.Component<NavProps> {
+  handleLogout = (e: any) => {
+    e.preventDefault();
+    this.props.apiStore!.logout();
+  };
+
+  render() {
+    const mainStore = this.props.mainStore!;
+    const apiStore = this.props.apiStore!;
+
+    return (
+      <Navbar color={'light'} light expand={'md'}>
+        <NavbarBrand href={'/'}>iZivi</NavbarBrand>
+        <NavbarToggler onClick={() => (mainStore.navOpen = !mainStore.navOpen)} />
+        <Collapse isOpen={mainStore.navOpen} navbar>
+          <Nav className={'ml-auto'} navbar>
+            {apiStore.isLoggedIn ? (
+              <>
+                <NavItem>
+                  <NavLink href="/logout" onClick={this.handleLogout}>
+                    Abmelden
+                  </NavLink>
+                </NavItem>
+              </>
+            ) : (
+              <>
+                <NavEntry to="/register">Registrieren</NavEntry>
+                <NavEntry to="/login">Anmelden</NavEntry>
+              </>
+            )}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
+}
