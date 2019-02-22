3a110586ecf46c1a762c163539954816df3bf58e
Philipp Fehr <philipp@thefehr.me>
2019-02-22 13:05:22 +0100

    Changed all sed's to global

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -44,9 +44,9 @@ BACKUP_DIR=./backup/izivi/$ENVIRONMENT
 cp frontend/htaccess.dist frontend/build/.htaccess && \
 ssh $TARGET mkdir -p ${PROJECT_DIR} ${PROJECT_DIR}.bak && \
 rsync -ra --exclude '.git' --exclude 'node_modules' . $TARGET:$TMP && \
-ssh $TARGET sed -i'' "s/COMMIT_ID/$TRAVIS_COMMIT/" $TMP/frontend/build/static/js/main.*.js && \
-ssh $TARGET sed -i'' "s/ENVIRONMENT/$ENVIRONMENT/" $TMP/frontend/build/static/js/main.*.js && \
-ssh $TARGET sed -i'' "s,BASE_URL,$API_URL," $TMP/frontend/build/static/js/main.*.js && \
+ssh $TARGET sed -i'' "s/COMMIT_ID/$TRAVIS_COMMIT/g" $TMP/frontend/build/static/js/main.*.js && \
+ssh $TARGET sed -i'' "s/ENVIRONMENT/$ENVIRONMENT/g" $TMP/frontend/build/static/js/main.*.js && \
+ssh $TARGET sed -i'' "s,BASE_URL,$API_URL,g" $TMP/frontend/build/static/js/main.*.js && \
 ssh $TARGET sed -i'' "s,SENTRY_DSN,$SENTRY_DSN_PUBLIC,g" $TMP/frontend/build/static/js/main.*.js && \
 ssh $TARGET cp $CONFIG_FILE $TMP/api/.env && \
 ssh $TARGET rm -r ${PROJECT_DIR}.bak && \
