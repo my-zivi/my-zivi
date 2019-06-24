77733f7bca8867326cc94a7a7b796003564b6fb2
Niels Saurer <me@nielssaurer.com>
2019-06-24 17:18:16 +0200

    Validate ExpenseSheet state changes (#86)
    * Validate ExpenseSheet State Changes
    
    * Specs for ExpenseSheet State Changes
    
    * Removed unneccessary statement
    
    * Adjust specs

--- api/app/models/expense_sheet.rb
+++ api/app/models/expense_sheet.rb
@@ -21,6 +21,8 @@ class ExpenseSheet < ApplicationRecord
             :paid_company_holiday_days,
             numericality: { only_integer: true }
 
+  validate :legitimate_state_change, if: :state_changed?
+
   enum state: {
     open: 0,
     ready_for_payment: 1,
@@ -30,5 +32,29 @@ class ExpenseSheet < ApplicationRecord
 
   def full_amount
     10_000 # TODO: implement correct calculator
+  end
+
+  private
+
+  def legitimate_state_change
+    return if [
+      state_ready_for_payment_or_open_to_ready_for_payment_or_open?,
+      state_ready_for_payment_to_payment_in_progress?,
+      state_payment_in_progress_to_paid_or_ready_for_payment?
+    ].one?
+
+    errors.add(:state, :invalid_state_change)
+  end
+
+  def state_ready_for_payment_or_open_to_ready_for_payment_or_open?
+    state.in?(%w[open ready_for_payment]) && state_was.in?(%w[open ready_for_payment])
+  end
+
+  def state_ready_for_payment_to_payment_in_progress?
+    state.in?(%w[payment_in_progress]) && state_was.in?(%w[ready_for_payment])
+  end
+
+  def state_payment_in_progress_to_paid_or_ready_for_payment?
+    state.in?(%w[paid ready_for_payment]) && state_was.in?(%w[payment_in_progress])
   end
 end
--- api/spec/models/expense_sheet_spec.rb
+++ api/spec/models/expense_sheet_spec.rb
@@ -24,4 +24,105 @@ RSpec.describe ExpenseSheet, type: :model do
   it_behaves_like 'validates that the ending is after beginning' do
     let(:model) { build(:expense_sheet, beginning: beginning, ending: ending) }
   end
+
+  describe '#state' do
+    subject { !expense_sheet.errors.added?(:state, :invalid_state_change) }
+
+    before { expense_sheet.update(state: state) }
+
+    let(:expense_sheet) do
+      expense_sheet = build(:expense_sheet, user: user, state: state_was)
+      expense_sheet.save validate: false
+      expense_sheet
+    end
+    let(:user) { create :user }
+
+    context 'when state was open' do
+      let(:state_was) { :open }
+
+      context 'when new state is ready_for_payment' do
+        let(:state) { :ready_for_payment }
+
+        it { is_expected.to eq true }
+      end
+
+      context 'when new state is payment_in_progress' do
+        let(:state) { :payment_in_progress }
+
+        it { is_expected.to eq false }
+      end
+
+      context 'when new state is paid' do
+        let(:state) { :paid }
+
+        it { is_expected.to eq false }
+      end
+    end
+
+    context 'when state was ready_for_payment' do
+      let(:state_was) { :ready_for_payment }
+
+      context 'when new state is open' do
+        let(:state) { :open }
+
+        it { is_expected.to eq true }
+      end
+
+      context 'when new state is payment_in_progress' do
+        let(:state) { :payment_in_progress }
+
+        it { is_expected.to eq true }
+      end
+
+      context 'when new state is paid' do
+        let(:state) { :paid }
+
+        it { is_expected.to eq false }
+      end
+    end
+
+    context 'when state was payment_in_progress' do
+      let(:state_was) { :payment_in_progress }
+
+      context 'when new state is open' do
+        let(:state) { :open }
+
+        it { is_expected.to eq false }
+      end
+
+      context 'when new state is ready_for_payment' do
+        let(:state) { :ready_for_payment }
+
+        it { is_expected.to eq true }
+      end
+
+      context 'when new state is paid' do
+        let(:state) { :paid }
+
+        it { is_expected.to eq true }
+      end
+    end
+
+    context 'when state was paid' do
+      let(:state_was) { :paid }
+
+      context 'when new state is open' do
+        let(:state) { :open }
+
+        it { is_expected.to eq false }
+      end
+
+      context 'when new state is ready_for_payment' do
+        let(:state) { :ready_for_payment }
+
+        it { is_expected.to eq false }
+      end
+
+      context 'when new state is payment_in_progress' do
+        let(:state) { :payment_in_progress }
+
+        it { is_expected.to eq false }
+      end
+    end
+  end
 end
