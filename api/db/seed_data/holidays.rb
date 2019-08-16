f5fd34122feaba8a1b1564611e423872f7c20c57
Niels Saurer <me@nielssaurer.com>
2019-08-16 08:44:23 +0200

    Simpler Seeds (#180)
    * Simplify ExpenseSheet seeds
    
    * Remove unnecessary merge in Holiday seeds
    
    * Extract common expenses in ServiceSpecification seeds
    
    * MOORE EXTRACTIONS

--- api/db/seed_data/holidays.rb
+++ api/db/seed_data/holidays.rb
@@ -2,8 +2,4 @@
 
 holidays = JSON.parse(File.read(Rails.root.join('db', 'seed_data', 'holidays.json')), symbolize_names: true)
 
-Holiday.create!(
-  holidays.map do |holiday|
-    holiday.merge(beginning: Date.parse(holiday[:beginning]), ending: Date.parse(holiday[:ending]))
-  end
-)
+Holiday.create!(holidays)
--- api/db/seed_data/service_specifications.rb
+++ api/db/seed_data/service_specifications.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+default_day_expenses = {
+  work_days_expenses: { breakfast: 400, lunch: 900, dinner: 700 },
+  paid_vacation_expenses: { breakfast: 400, lunch: 900, dinner: 700 },
+  first_day_expenses: { breakfast: 0, lunch: 900, dinner: 700 },
+  last_day_expenses: { breakfast: 400, lunch: 900, dinner: 0 }
+}
+
 ServiceSpecification.create!(
   [
     {
@@ -8,10 +15,6 @@ ServiceSpecification.create!(
       short_name: 'F',
       work_clothing_expenses: 230,
       accommodation_expenses: 0,
-      work_days_expenses: { breakfast: 400, lunch: 900, dinner: 700 },
-      paid_vacation_expenses: { breakfast: 400, lunch: 900, dinner: 700 },
-      first_day_expenses: { breakfast: 0, lunch: 900, dinner: 700 },
-      last_day_expenses: { breakfast: 400, lunch: 900, dinner: 0 },
       location: :zurich,
       active: true
     },
@@ -21,10 +24,6 @@ ServiceSpecification.create!(
       short_name: 'F',
       work_clothing_expenses: 0,
       accommodation_expenses: 0,
-      work_days_expenses: { breakfast: 400, lunch: 900, dinner: 700 },
-      paid_vacation_expenses: { breakfast: 400, lunch: 900, dinner: 700 },
-      first_day_expenses: { breakfast: 0, lunch: 900, dinner: 700 },
-      last_day_expenses: { breakfast: 400, lunch: 900, dinner: 0 },
       location: :zurich,
       active: true
     },
@@ -34,12 +33,8 @@ ServiceSpecification.create!(
       short_name: 'W',
       work_clothing_expenses: 230,
       accommodation_expenses: 0,
-      work_days_expenses: { breakfast: 400, lunch: 900, dinner: 700 },
-      paid_vacation_expenses: { breakfast: 400, lunch: 900, dinner: 700 },
-      first_day_expenses: { breakfast: 0, lunch: 900, dinner: 700 },
-      last_day_expenses: { breakfast: 400, lunch: 900, dinner: 0 },
       location: :valais,
       active: true
     }
-  ]
+  ].map { |service_specification| service_specification.merge(default_day_expenses) }
 )
