c0102f9fd437949788f3ee7daad2478e03264c1a
Philipp Fehr <philipp@thefehr.me>
2019-09-05 12:00:24 +0200

    Improve PAIN xml (#231)
    * Pain xml is now disposition: attachment, and got a filename
    
    * Linting

--- api/app/controllers/v1/payments_controller.rb
+++ api/app/controllers/v1/payments_controller.rb
@@ -17,7 +17,9 @@ module V1
           render :show
         end
         format.xml do
-          render plain: generate_pain, content_type: :xml
+          send_data generate_pain, disposition: 'attachment',
+                                   filename: I18n.t('payment.pain_filename',
+                                                    from_date: I18n.l(@payment.payment_timestamp, format: '%d.%m.%Y'))
         end
       end
     end
