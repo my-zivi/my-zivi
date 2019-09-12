29fc315a7a849ad86a68aa8c2264aa28aa3f8339
Philipp Fehr <philipp@thefehr.me>
2019-09-12 23:24:26 +0200

    Improve cypress mock server (#262)

--- frontend/cypress/server/index.js
+++ frontend/cypress/server/index.js
@@ -23,24 +23,48 @@ server.post('/v1/users/validate', (_, res) => {
   res.send('');
 });
 
-function appendAuthorizationHeader(res) {
-  res.set('Authorization', defaultValues.authorization_token);
+function handleLogin(res, req, statusCode = 200) {
+  let user = defaultUser;
+  let authHeader = defaultValues.authorization_token;
+
+  appendAccessControlHeaders(res);
+
+  if (req.headers.authorization !== undefined) {
+    const jwtContent = parseJwt(req.headers.authorization);
+
+    if (jwtContent.isAdmin) {
+      user = require('./db').user[1]
+      authHeader = defaultValues.admin_authorization_token;
+    }
+  }
+
+  appendAuthorizationHeader(res, authHeader);
+  res.status(statusCode);
+  res.jsonp(user)
+}
+
+function parseJwt (token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+};
+
+function appendAuthorizationHeader(res, authorization_token) {
+  res.set('Authorization', authorization_token);
 }
 
 server.post('/v1/users', (req, res) => {
   console.log('POST /v1/users 201 :created');
-  appendAccessControlHeaders(res);
-  appendAuthorizationHeader(res);
-  res.status(201);
-  res.send(JSON.stringify(defaultUser));
+  handleLogin(res, req, 201);
 });
 
 server.post('/v1/users/sign_in', (req, res) => {
   console.log('POST /v1/users/sign_in 200 :ok');
-  appendAccessControlHeaders(res);
-  appendAuthorizationHeader(res);
-  res.status(200);
-  res.jsonp(defaultUser);
+  handleLogin(res, req);
 });
 
 server.use(jsonServer.defaults());
