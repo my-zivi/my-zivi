f49d9f9442830289b247c69eb69cbd039b334638
Lukas Bischof <me@luk4s.dev>
2020-06-11 09:52:14 +0200

    Rubocop

--- Rakefile
+++ Rakefile
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 # Add your own tasks in files placed in lib/tasks ending in .rake,
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
 require_relative 'config/application'
 
 Rails.application.load_tasks
+
+Rake::Task['assets:clean'].enhance do
+  FileUtils.remove_dir('node_modules', true)
+  FileUtils.remove_dir('vendor/javascript', true)
+  FileUtils.remove_dir('tmp/cache/webpacker', true)
+end
--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
 # require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -23,6 +25,9 @@ module MyZivi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'Zurich'
+    config.i18n.default_locale = :de
+    config.i18n.available_locales = %i[de fr]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
