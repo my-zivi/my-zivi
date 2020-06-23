04dae8f6568234dd6d2a3d97406d86d2c0458aab
Lukas Bischof <me@luk4s.dev>
2020-06-23 19:04:01 +0200

    Updated i18n tasks config

--- spec/config/i18n_spec.rb
+++ spec/config/i18n_spec.rb
@@ -5,6 +5,7 @@ require 'i18n/tasks'
 RSpec.describe I18n do
   let(:i18n) { I18n::Tasks::BaseTask.new }
   let(:missing_keys) { i18n.missing_keys }
+  let(:unused_keys) { i18n.unused_keys }
   let(:inconsistent_interpolations) { i18n.inconsistent_interpolations }
 
   it 'does not have missing keys' do
@@ -18,6 +19,10 @@ RSpec.describe I18n do
                     "#{non_normalized.map { |path| "  #{path}" }.join("\n")}\n" \
                     "Please run `i18n-tasks normalize' to fix"
     expect(non_normalized).to be_empty, error_message
+  end
+
+  it 'does not have unused keys' do
+    expect(unused_keys).to be_empty, "#{unused_keys.leaves.count} unused i18n keys"
   end
 
   it 'does not have inconsistent interpolations' do
