24a9957ad9da40f1ca735d2eb495edf160025466
Lukas Bischof <me@luk4s.dev>
2020-06-27 12:11:30 +0200

    Fix él Spec

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Homes', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/home/index'
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
