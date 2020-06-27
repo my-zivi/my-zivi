1cbc16842b62372e35046f18d3d80212f9e12fc8
Lukas Bischof <me@luk4s.dev>
2020-06-27 12:48:13 +0200

    Theme (#33)

---
+++ app/javascript/images/theme/generic/index.js
@@ -0,0 +1,3 @@
+import '@my-zivi/falcon/pages/assets/img/generic/bg-1.jpg';
+import '@my-zivi/falcon/pages/assets/img/generic/bg-2.jpg';
+import '@my-zivi/falcon/pages/assets/img/generic/dashboard-alt.png';
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   root 'home#index'
+  devise_for :users
 end
