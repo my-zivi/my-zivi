21ab5894948cd334455bcc5dc84bf3a4e715feb1
Philipp Fehr <philipp@thefehr.me>
2020-07-02 09:30:41 +0200

    ReAdd all services

---
+++ app/services/pdfs/expenses_overview_service.rb
@@ -0,0 +1,156 @@
+# frozen_string_literal: true
+
+require 'prawn'
+
+# require_relative 'expenses_overview/expenses_overview_additions'
+
+module Pdfs
+  # rubocop:disable ClassLength
+  class ExpensesOverviewService
+    include Prawn::View
+    include Pdfs::PrawnHelper
+
+    def initialize(service_specifications, dates)
+      @beginning = dates.beginning
+      @ending = dates.ending
+      @service_specifications = service_specifications
+      update_font_families
+      headline
+      header
+      content_table
+    end
+
+    def document
+      @document ||= Prawn::Document.new(page_size: 'A4', page_layout: :landscape)
+    end
+
+    private
+
+    def headline
+      text I18n.t('pdfs.expenses_overview.swo', date: I18n.l(Time.zone.today)), align: :right, size: 8
+      text I18n.t('pdfs.expenses_overview.basedon', date: I18n.l(Time.zone.today)), align: :right, size: 8
+      text(I18n.t('pdfs.expenses_overview.title', beginning: I18n.l(@beginning), ending: I18n.l(@ending)),
+           align: :left, style: :bold, size: 15)
+    end
+
+    def header
+      font_size 9
+      table([Pdfs::ExpensesOverview::ExpensesOverviewAdditions::TABLE_HEADER,
+             Pdfs::ExpensesOverview::ExpensesOverviewAdditions::TABLE_SUB_HEADER],
+            cell_style: { borders: [] }, width: bounds.width,
+            column_widths: Pdfs::ExpensesOverview::ExpensesOverviewAdditions::COLUMN_WIDTHS) do
+        row(0).font_style = :bold
+      end
+    end
+
+    # rubocop:disable Metrics/AbcSize
+    def content_table
+      total_days = 0
+      total_expenses = 0.0
+      font_size 9
+      @service_specifications.values.each do |expense_sheet|
+        # rubocop:disable Metrics/LineLength
+        table(table_data(expense_sheet), cell_style: { borders: [], padding: [0, 5, 0, 5] }, width: bounds.width, column_widths: Pdfs::ExpensesOverview::ExpensesOverviewAdditions::COLUMN_WIDTHS)
+        # rubocop:enable Metrics/LineLength
+        sum_table(expense_sheet)
+        total_days += (expense_sheet.sum(&:work_days) + expense_sheet.sum(&:workfree_days) +
+        expense_sheet.sum(&:paid_vacation_days) + expense_sheet.sum(&:sick_days))
+        total_expenses += expense_sheet.sum(&:calculate_full_expenses)
+      end
+      total_sum_table(total_days, total_expenses)
+    end
+
+    # rubocop:enable Metrics/AbcSize
+    # :reek:FeatureEnvy
+    def sum_table(expense_sheets)
+      table([[{ content: 'Gesamt: ', align: :left },
+              { content: (expense_sheets.sum(&:work_days) + expense_sheets.sum(&:workfree_days) +
+                expense_sheets.sum(&:paid_vacation_days) + expense_sheets.sum(&:sick_days)).to_s,
+                align: :right },
+              { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheets.sum(&:calculate_full_expenses).to_s),
+                align: :right }]], cell_style: { borders: [], padding: [1, 5, 1, 5] },
+                                   header: false, position: :right, column_widths: [40, 30, 45]) do
+        row(0).font_style = :bold
+      end
+    end
+
+    def total_sum_table(total_days, total_expenses)
+      table([[{ content: 'Total Tage: ' + total_days.to_s + ', Total Betrag: ' +
+        Pdfs::ExpenseSheet::FormatHelper.to_chf(total_expenses.to_s), align: :right }]],
+            cell_style: { borders: [] }, header: false, position: :right) do
+        row(0).font_style = :italic
+      end
+    end
+
+    def pre_table(name)
+      move_down 25
+      text(name, align: :left, style: :bold, size: 11)
+    end
+
+    def table_data(expense_sheets)
+      move_down 10
+      table_content(expense_sheets)
+    end
+
+    def first_part(expense_sheet)
+      exps_user = expense_sheet.user
+      [{ content: exps_user.zdp.to_s, align: :right },
+       { content: (exps_user.last_name + ' ' + exps_user.first_name) },
+       { content: (I18n.l(expense_sheet.beginning, format: :short) + ' - ' +
+         I18n.l(expense_sheet.ending, format: :short)).to_s, align: :center }]
+    end
+
+    def second_part(expense_sheet)
+      # rubocop:disable Metrics/LineLength
+      [{ content: expense_sheet.work_days.to_s, align: :right }, { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.calculate_work_days[:total] + expense_sheet.calculate_first_day[:total] + expense_sheet.calculate_last_day[:total]), align: :right },
+       # rubocop:enable Metrics/LineLength
+       { content: expense_sheet.workfree_days.to_s, align: :right }]
+    end
+
+    def third_part(expense_sheet)
+      [{ content: Pdfs::ExpenseSheet::FormatHelper.to_chf(
+        expense_sheet.calculate_workfree_days[:total]
+      ), align: :right },
+       { content: expense_sheet.sick_days.to_s, align: :right },
+       { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.calculate_sick_days[:total]), align: :right },
+       { content: expense_sheet.paid_vacation_days.to_s, align: :right },
+       { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(
+         expense_sheet.calculate_paid_vacation_days[:total]
+       ), align: :right }]
+    end
+
+    def fourth_part(expense_sheet)
+      [{ content: expense_sheet.unpaid_vacation_days.to_s, align: :right },
+       { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.calculate_unpaid_vacation_days[:total]),
+         align: :right },
+       { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.driving_expenses), align: :right },
+       { content: (expense_sheet.work_days + expense_sheet.workfree_days +
+        expense_sheet.paid_vacation_days + expense_sheet.sick_days).to_s, align: :right }]
+    end
+
+    def fifth_part(expense_sheet)
+      [
+        { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.clothing_expenses), align: :right },
+        { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.extraordinary_expenses), align: :right },
+        { content: (expense_sheet.work_days + expense_sheet.workfree_days +
+          expense_sheet.paid_vacation_days + expense_sheet.sick_days).to_s, align: :right }
+      ]
+    end
+
+    def sixt_part(expense_sheet)
+      [
+        { content: Pdfs::ExpenseSheet::FormatHelper.to_chf(expense_sheet.calculate_full_expenses.to_d), align: :right }
+      ]
+    end
+
+    def table_content(expense_sheets)
+      expense_sheets.map do |expense_sheet|
+        expense_sheet.slice.values
+        # rubocop:disable Metrics/LineLength
+        first_part(expense_sheet) + second_part(expense_sheet) + third_part(expense_sheet) + fourth_part(expense_sheet) + fifth_part(expense_sheet) + sixt_part(expense_sheet)
+        # rubocop:enable Metrics/LineLength
+      end
+    end
+  end
+  # rubocop:enable ClassLength
+end
---
+++ app/services/pdfs/service_agreement/glue_service.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'pdf_forms'
+require 'hexapdf'
+
+module Pdfs
+  module ServiceAgreement
+    class GlueService
+      FRENCH_FILE_PATH = Rails.root.join('app', 'assets', 'pdfs', 'french_service_agreement_text.pdf').freeze
+      GERMAN_FILE_PATH = Rails.root.join('app', 'assets', 'pdfs', 'german_service_agreement_text.pdf').freeze
+
+      def initialize(service)
+        @service = service
+        @combined = HexaPDF::Document.new
+      end
+
+      def render
+        fill_and_load_form
+        load_info_text
+        generate_and_load_first_page
+
+        pdf_io = StringIO.new
+        @combined.write(pdf_io)
+        pdf_io.string
+      end
+
+      private
+
+      def generate_and_load_first_page
+        ioify_and_combine(FirstPage.new(@service))
+      end
+
+      def fill_and_load_form
+        ioify_and_combine(FormFiller.new(@service))
+      end
+
+      def ioify_and_combine(pdf)
+        pdf_io = StringIO.new(pdf.render)
+
+        HexaPDF::Document.new(io: pdf_io).pages.each { |page| @combined.pages << @combined.import(page) }
+      end
+
+      def load_info_text
+        HexaPDF::Document.open(
+          valais? ? FRENCH_FILE_PATH : GERMAN_FILE_PATH
+        ).pages.each { |page| @combined.pages << @combined.import(page) }
+      end
+
+      def valais?
+        @service.service_specification.location_valais?
+      end
+    end
+  end
+end
