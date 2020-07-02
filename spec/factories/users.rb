a4588534388bd732ac604d1823876f2ca3bdeead
Lukas Bischof <me@luk4s.dev>
2020-07-02 15:54:10 +0200

    Fix factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "example#{n}@example.test" }
     password { '12345678' }
+
+    trait :confirmed do
+      confirmed_at { 5.minutes.ago }
+    end
   end
 end
