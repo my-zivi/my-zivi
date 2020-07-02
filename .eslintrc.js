6111f9865e2d728da0d71fd868f7876a1fc9edff
Lukas Bischof <me@luk4s.dev>
2020-07-02 10:08:44 +0200

    Pre push hook and Eslint Setup (#45)
    * Pre push hook
    
    * Add eslint
    
    * Fix eslint issues
    
    * Adapt bin/lint
    
    * Update semaphore

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    "max-len": [2, 120, 4],
+  },
+};
