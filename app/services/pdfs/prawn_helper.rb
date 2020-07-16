8da275b4181876906dd5280ff778bc72618ad6e1
Philipp Fehr <philipp@thefehr.me>
2020-07-16 10:58:04 +0200

    Add some fooonts

--- app/services/pdfs/prawn_helper.rb
+++ app/services/pdfs/prawn_helper.rb
@@ -10,20 +10,20 @@ module Pdfs
     end
 
     def update_font_families
-      # font_families.update(
-      #   'RobotoCondensed' => {
-      #     normal: font_file_path('RobotoCondensed-Regular.ttf'),
-      #     bold: font_file_path('RobotoCondensed-Bold.ttf'),
-      #     italic: font_file_path('RobotoCondensed-RegularItalic.ttf')
-      #   }
-      # )
-      # font 'RobotoCondensed'
+      font_families.update(
+        'Roboto' => {
+          normal: font_file_path('Roboto-Regular.ttf'),
+          bold: font_file_path('Roboto-Bold.ttf'),
+          italic: font_file_path('Roboto-Italic.ttf')
+        }
+      )
+      font 'Roboto'
     end
 
     private
 
     def font_file_path(filename)
-      Rails.root.join('app/assets/fonts', filename)
+      Rails.root.join('app/javascript/fonts', filename)
     end
   end
 end
