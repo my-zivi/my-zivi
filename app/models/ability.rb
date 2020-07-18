0a8c087e1cb82206946eb02c15bc28b61a133da5
Lukas Bischof <me@luk4s.dev>
2020-07-18 13:06:04 +0200

    Make reek "happy"

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,13 +3,15 @@
 class Ability
   include CanCan::Ability
 
+  # :reek:FeatureEnvy
   def initialize(user)
     return if user.blank?
 
-    if user.referencee.is_a? CivilServant
-      merge(Abilities::CivilServantAbility.new(user.referencee))
-    elsif user.referencee.is_a? OrganizationMember
-      merge(Abilities::OrganizationMemberAbility.new(user.referencee))
+    referencee = user.referencee
+    if referencee.is_a? CivilServant
+      merge(Abilities::CivilServantAbility.new(referencee))
+    elsif referencee.is_a? OrganizationMember
+      merge(Abilities::OrganizationMemberAbility.new(referencee))
     end
   end
 end
