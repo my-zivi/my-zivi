f97b3f51962d02a12920c4d919b61f16702361f3
Lukas Bischof <me@luk4s.dev>
2020-07-23 22:55:35 +0200

    View specs

--- spec/views/organizations/service_specifications/edit.html.slim_spec.rb
+++ spec/views/organizations/service_specifications/edit.html.slim_spec.rb
@@ -6,41 +6,12 @@ RSpec.describe 'organizations/service_specifications/edit', type: :view do
   let(:service_specification) { create(:service_specification) }
   let(:current_organization_admin) { build(:organization_member) }
 
-  let(:expected_input_names) do
-    %i[
-      name internal_note work_clothing_expenses
-      accommodation_expenses location active identification_number
-    ]
-  end
-
-  let(:expected_expense_fields) do
-    %i[work_days_expenses paid_vacation_expenses first_day_expenses last_day_expenses]
-  end
-
   before do
     assign(:service_specification, service_specification)
     allow(view).to receive(:current_organization_admin).and_return current_organization_admin
-    render
   end
 
-  it 'renders the edit text based input fields' do
-    assert_select 'form[action=?][method=?]', organizations_service_specification_path(service_specification), 'post' do
-      expected_input_names.each do |name|
-        assert_select 'input[name=?]', "service_specification[#{name}]"
-      end
-    end
-  end
-
-  it 'renders organization member relation fields' do
-    assert_select 'select[name=?]', 'service_specification[contact_person_id]'
-    assert_select 'select[name=?]', 'service_specification[lead_person_id]'
-  end
-
-  it 'renders expense fields' do
-    expected_expense_fields.each do |name|
-      assert_select 'input[name=?]', "service_specification[#{name}][breakfast]"
-      assert_select 'input[name=?]', "service_specification[#{name}][lunch]"
-      assert_select 'input[name=?]', "service_specification[#{name}][dinner]"
-    end
+  it_behaves_like 'renders service specification form' do
+    let(:form_action) { organizations_service_specification_path(service_specification) }
   end
 end
--- spec/views/organizations/service_specifications/new.html.slim_spec.rb
+++ spec/views/organizations/service_specifications/new.html.slim_spec.rb
@@ -3,14 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe 'organizations/service_specifications/new', type: :view do
+  let(:service_specification) { ServiceSpecification.new }
+  let(:current_organization_admin) { build(:organization_member) }
+
   before do
-    assign(:organizations_service_specification, ServiceSpecification.new)
+    assign(:service_specification, service_specification)
+    allow(view).to receive(:current_organization_admin).and_return current_organization_admin
   end
 
-  it 'renders new organizations_service_specification form' do
-    render
-
-    assert_select 'form[action=?][method=?]', organizations_service_specifications_path, 'post' do
-    end
+  it_behaves_like 'renders service specification form' do
+    let(:form_action) { organizations_service_specifications_path }
   end
 end
