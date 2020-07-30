1778b8425ee423a0e43af90363ebb9c917cf1a1a
Lukas Bischof <me@luk4s.dev>
2020-07-30 22:16:28 +0200

    Specs and translations

--- app/controllers/mailing_lists_controller.rb
+++ app/controllers/mailing_lists_controller.rb
@@ -4,18 +4,20 @@ class MailingListsController < ApplicationController
   authorize_resource
 
   def create
-    if MailingList.create(mailing_list_params)
-      flash[:success] = 'Blubby'
-    else
-      flash[:error] = 'Blubbs'
-    end
+    @mailing_list = MailingList.new(mailing_list_params)
 
-    redirect_to root_path
+    if @mailing_list.save
+      flash[:mailing_list_success] = I18n.t('mailing_list.successful_creation')
+      redirect_to root_path(anchor: 'mailing-list')
+    else
+      flash[:mailing_list_error] = I18n.t('mailing_list.erroneous_creation')
+      render 'home/index'
+    end
   end
 
   private
 
   def mailing_list_params
-    params.require(:mailing_list).permit(:email)
+    params.require(:mailing_list).permit(:email, :name, :organization, :telephone)
   end
 end
---
+++ spec/requests/mailing_lists_controller_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MailingListsController, type: :request do
+  describe '#create' do
+    let(:perform_request) { post mailing_list_path, params: { mailing_list: mailing_list_params } }
+    let(:mailing_list_params) do
+      {
+        name: 'Lucy Hopcraft',
+        organization: 'MyZivi AG',
+        email: 'lucy@example.com',
+        telephone: '0798921234'
+      }.with_indifferent_access
+    end
+
+    let(:created_mailing_list_entry) { MailingList.order(:created_at).last }
+
+    it 'creates mailing list inquiry and returns a successful response' do
+      expect { perform_request }.to(change(MailingList, :count))
+      expect(created_mailing_list_entry.slice(mailing_list_params.keys)).to eq mailing_list_params
+      expect(response).to redirect_to root_path(anchor: 'mailing-list')
+      follow_redirect!
+      expect(response.body).to include I18n.t('mailing_list.successful_creation')
+    end
+
+    context 'when params are invalid' do
+      let(:mailing_list_params) do
+        {
+          name: '',
+          organization: 'MyZivi AG',
+          email: '',
+          telephone: '0798921234'
+        }
+      end
+
+      it 'does not create a mailing list inquiry and returns an error message' do
+        expect { perform_request }.not_to change(MailingList, :count)
+        expect(response).to render_template 'home/index'
+        expect(response.body).to include I18n.t('mailing_list.erroneous_creation')
+      end
+    end
+  end
+end
