0fd7d5cd63d8889eeaec99b8bb4e12efa25496f8
Lukas Bischof <me@luk4s.dev>
2020-07-30 22:33:50 +0200

    Fix stuff

--- spec/factories/mailing_lists.rb
+++ spec/factories/mailing_lists.rb
@@ -5,8 +5,8 @@ require 'faker'
 FactoryBot.define do
   factory :mailing_list do
     email { Faker::Internet.email }
-    organization { Faker::Company.name }
-    telephone { Faker::PhoneNumber.cell_phone }
-    name { Faker::Name.name }
+    organization { 'MyZivi AG' }
+    telephone { '+41 79 892 12 34' }
+    name { 'Maximilian Muster' }
   end
 end
