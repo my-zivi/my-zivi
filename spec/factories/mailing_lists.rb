f96f99ac13485fe42d7a4b7eadca01ce6f8da15b
Lukas Bischof <me@luk4s.dev>
2020-07-30 22:38:27 +0200

    More fix

--- spec/factories/mailing_lists.rb
+++ spec/factories/mailing_lists.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-require 'faker'
-
 FactoryBot.define do
   factory :mailing_list do
-    email { Faker::Internet.email }
+    sequence(:email) { |i| "email#{i}@example.com" }
     organization { 'MyZivi AG' }
     telephone { '+41 79 892 12 34' }
     name { 'Maximilian Muster' }
