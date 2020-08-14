4b6024b5c5fdba2ebf155495543e01bd38adbb6f
Lukas Bischof <me@luk4s.dev>
2020-08-14 14:36:35 +0200

    Rubocop

--- spec/requests/organizations/civil_servants_request_spec.rb
+++ spec/requests/organizations/civil_servants_request_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Organizations::CivilServants", type: :request do
-
-  describe "GET /index" do
-    it "returns http success" do
-      get "/organizations/civil_servants/index"
+RSpec.describe 'Organizations::CivilServants', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/organizations/civil_servants/index'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
