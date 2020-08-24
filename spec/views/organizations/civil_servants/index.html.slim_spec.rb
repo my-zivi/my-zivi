cc4fa362d0f7b236f544d9aab0165aa4511f55a2
Lukas Bischof <me@luk4s.dev>
2020-08-24 15:43:19 +0200

    View spec

--- spec/views/organizations/civil_servants/index.html.slim_spec.rb
+++ spec/views/organizations/civil_servants/index.html.slim_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe 'organizations/civil_servants/index.html.slim', type: :view do
   subject { rendered }
 
+  let(:show_inactive) { true }
   let(:civil_servants) do
     [
       build(:civil_servant, first_name: 'Peter', last_name: 'Black', id: 1),
@@ -14,6 +15,7 @@ RSpec.describe 'organizations/civil_servants/index.html.slim', type: :view do
 
   before do
     assign(:civil_servants, civil_servants)
+    assign(:filters, { show_inactive: show_inactive })
     render
   end
 
@@ -21,5 +23,15 @@ RSpec.describe 'organizations/civil_servants/index.html.slim', type: :view do
     expect(rendered).to include '(2)'
     expect(rendered).to include 'Peter Black', 'PB', organizations_civil_servant_path(1)
     expect(rendered).to include 'Cordula Grün', 'CG', organizations_civil_servant_path(2)
+
+    assert_select 'input[id=?][checked]', 'hide-past-services'
+  end
+
+  context 'when show inactive is false' do
+    let(:show_inactive) { false }
+
+    it 'renders unchecked input field' do
+      assert_select 'input[id=?]:not([checked])', 'hide-past-services'
+    end
   end
 end
