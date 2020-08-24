0b9aef0dfef151a88cafa65399353177cf76a38b
Philipp Fehr <philipp@thefehr.me>
2020-08-24 10:16:12 +0200

    Remove short name length validation, correct seeds and factory (#97)

--- app/models/regional_center.rb
+++ app/models/regional_center.rb
@@ -2,7 +2,6 @@
 
 class RegionalCenter < ApplicationRecord
   validates :name, :short_name, presence: true
-  validates :short_name, length: { is: 2 }
 
   has_many :civil_servants, dependent: :restrict_with_error, inverse_of: :regional_center
   belongs_to :address, dependent: :destroy
