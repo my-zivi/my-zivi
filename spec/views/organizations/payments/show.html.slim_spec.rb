0be758286412d7dc7c9b8c974a7bd3874d4b649a
Lukas Bischof <me@luk4s.dev>
2020-08-27 13:34:22 +0200

    Show spec

--- spec/views/organizations/payments/show.html.slim_spec.rb
+++ spec/views/organizations/payments/show.html.slim_spec.rb
@@ -2,6 +2,27 @@
 
 require 'rails_helper'
 
-RSpec.describe 'payments/show.html.slim', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'organizations/payments/show.html.slim', type: :view do
+  subject { rendered }
+
+  let(:civil_servant) { create(:civil_servant, :full, :with_service) }
+
+  let(:payment) do
+    build(:payment,
+          id: 1,
+          created_at: '2020-04-01',
+          amount: 12,
+          expense_sheets: build_pair(:expense_sheet, id: 2, service: civil_servant.services.first))
+  end
+
+  before do
+    assign(:payment, payment)
+    render
+  end
+
+  it 'does render' do
+    expect(rendered).to include I18n.t('organizations.payments.show.title', date: '01.04.2020'), 'CHF 12.00'
+    expect(rendered).to include I18n.t('organizations.payments.show.expense_sheets_table.title')
+    expect(rendered.scan(/Zivi Mustermann/).length).to eq 2
+  end
 end
