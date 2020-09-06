6a96ebbb611e750bf5f788ab36d857b7cee502e4
Lukas Bischof <me@luk4s.dev>
2020-09-06 18:34:21 +0200

    Rubocop

--- spec/views/organizations/planning/index.html.slim_spec.rb
+++ spec/views/organizations/planning/index.html.slim_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "planning/index.html.slim", type: :view do
+RSpec.describe 'planning/index.html.slim', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
