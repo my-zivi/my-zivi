c8d5a9c91bd1970de780490207ff2c923c1f25a3
Philipp Fehr <philipp@thefehr.me>
2020-09-11 18:06:59 +0200

    Saving files before refreshing line endings

---
+++ spec/views/organizations/overview/index.html.slim_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'organizations/overview/index.html.slim', type: :view do
+  subject { rendered }
+
+  let(:current_organization_admin) { build(:organization_member) }
+
+  before do
+    allow(view).to receive(:current_organization_admin).and_return current_organization_admin
+    render
+  end
+
+  it 'renders the welcome card' do
+    expect(rendered).to include t('organizations.overview.index.welcome_back.title', name: current_organization_admin.full_name)
+  end
+end
