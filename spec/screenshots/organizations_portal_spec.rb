780cace28d08e2a10c7c085716a84e9063ee75d7
Lukas Bischof <me@luk4s.dev>
2020-09-12 10:42:13 +0200

    Basic System Specs (#128)
    * Some system specs
    
    * Civil Servants portal walkthrough spec
    
    * Adapt CI Pipeline
    
    * Capture screenshot of org dashboard
    
    * Remove randomness
    
    * Make it truly current

---
+++ spec/screenshots/organizations_portal_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'percy'
+
+RSpec.describe 'Organizations Portal Screenshots', type: :system, js: true do
+  let(:organization_administrator) { create(:organization_member, first_name: 'Therese', last_name: 'Mayer') }
+
+  before do
+    sign_in organization_administrator.user
+  end
+
+  it 'renders front page correctly' do
+    visit organizations_path
+    Percy.snapshot(page, { name: 'Organizations Dashboard' })
+  end
+end
