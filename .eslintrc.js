ea51c8b23f86d7dce8eaae19788b5ba4d6c37b83
Lukas Bischof <lukas@myzivi.ch>
2020-09-13 18:52:35 +0200

    Lint typescript files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,16 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2020: true,
   },
   extends: [
     'airbnb-base',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint'
   ],
   parserOptions: {
     ecmaVersion: 11,
