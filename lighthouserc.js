45ca04f637646b7993612bba7edb96573c3d6ebb
Lukas Bischof <lukas@myzivi.ch>
2020-09-17 20:12:10 +0200

    Binstubs

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: ['http://localhost:3000/'],
-      startServerCommand: 'NO_SSL=true rails server -e production',
+      startServerCommand: 'NO_SSL=true bin/rails server -e production',
     },
     upload: {
       target: 'temporary-public-storage',
