e9db5ea2fe111a568e587a38386b261520937ec7
Lukas Bischof <lukas@myzivi.ch>
2020-09-18 22:23:38 +0200

    Automatically optimise images

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,8 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const environment = require('./environment')
+const environment = require('./environment');
+const imageLoader = require('./loaders/image-webpack-loader');
 
-module.exports = environment.toWebpackConfig()
+environment.loaders.append('image-webpack-loader', imageLoader(false));
+
+module.exports = environment.toWebpackConfig();
