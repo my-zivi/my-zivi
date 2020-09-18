07a2c89eba5f2b5a9ead5ba2366bba6143136620
Lukas Bischof <lukas@myzivi.ch>
2020-09-18 22:23:02 +0200

    Specify Cache-Control header

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,4 +115,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=#{180.days.to_i}"
+  }
 end
