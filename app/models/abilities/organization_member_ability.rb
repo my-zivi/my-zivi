d432fc4fce51c6db0e344b5c4b652e2abe37a444
Philipp Fehr <philipp.fehr@myzivi.ch>
2020-09-29 21:30:37 +0200

    Fix abilities

--- app/models/abilities/organization_member_ability.rb
+++ app/models/abilities/organization_member_ability.rb
@@ -22,21 +22,7 @@ module Abilities
               organization_id: organization_id
             }
           })
-
-      can(:manage, Service, {
-            civil_servant_agreed: true,
-            organization_agreed: true,
-            service_specification: {
-              organization_id: organization_id
-            }
-          })
-      can(:manage, Service, {
-            organization_agreed: true,
-            service_specification: {
-              organization_id: organization_id
-            }
-          })
-
+      
       can(:manage, ExpenseSheet, {
             service: {
               civil_servant_agreed: true,
@@ -46,8 +32,29 @@ module Abilities
               }
             }
           })
+
+      service_abilities(organization_id)
     end
 
     # rubocop:enable Metrics/MethodLength
+    private
+
+    def service_abilities(organization_id)
+      can(:read, Service, {
+          civil_servant_agreed: true,
+          organization_agreed: true,
+          service_specification: {
+              organization_id: organization_id
+          }
+      })
+
+      can(:manage, Service, {
+          civil_servant_agreed: false,
+          organization_agreed: true,
+          service_specification: {
+              organization_id: organization_id
+          }
+      })
+    end
   end
 end
