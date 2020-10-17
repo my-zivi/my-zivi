eec3bfa4226b16d3b0ddc94396ade7ee6c44a312
Philipp Fehr <philipp@thefehr.me>
2020-10-17 11:30:57 +0200

    Make User invitable (#155)
    * Make User invitable
    
    * Add translations and set behaviour as before

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   belongs_to :referencee, polymorphic: true
 
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :trackable
 
