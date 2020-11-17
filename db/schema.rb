5717b3eda950c990c54a13764d6756b717d08735
Lukas Bischof <lukas@myzivi.ch>
2020-11-17 17:20:34 +0100

    Fix @TheFehr's PullRequest #142

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_24_145705) do
+ActiveRecord::Schema.define(version: 2020_11_17_161845) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
