28e584700061a66783bb14a787bc371f2fadc10f
Lukas Bischof <lukas@myzivi.ch>
2020-11-28 20:06:01 +0100

    Add a service

--- spec/screenshots/organizations_portal_spec.rb
+++ spec/screenshots/organizations_portal_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe 'Organizations Portal Screenshots', type: :system, js: true do
   let(:organization_administrator) { create(:organization_member, first_name: 'Therese', last_name: 'Mayer') }
 
   before do
+    service_specification = create(:service_specification, organization: organization_administrator.organization)
+    create(:service, service_specification: service_specification)
+
     sign_in organization_administrator.user
   end
 
