d49ea94dac7b35270a1031377d93a57f42ca1df3
Lukas Bischof <lukas@myzivi.ch>
2020-11-28 19:54:02 +0100

    Fixes

--- app/helpers/organizations/overview_helper.rb
+++ app/helpers/organizations/overview_helper.rb
@@ -27,16 +27,16 @@ module Organizations
     end
 
     # @param [Service] service
-    # rubocop:disable Metrics/AbcSize
     def service_state_label(service)
+      civil_servant_agreed = service.civil_servant_agreed?
+
       {
-        (service.civil_servant_agreed? && service.confirmation_date.present?) => I18n.t('service_states.active'),
-        (service.civil_servant_agreed? && service.confirmation_date.nil?) => I18n.t('service_states.pending_contract'),
+        (civil_servant_agreed && service.confirmation_date.present?) => I18n.t('service_states.active'),
+        (civil_servant_agreed && service.confirmation_date.nil?) => I18n.t('service_states.pending_contract'),
         service.civil_servant_decided_at.nil? => I18n.t('service_states.waiting_for_civil_servant_decision'),
-        (service.civil_servant_decided_at.present? && !service.civil_servant_agreed?) =>
+        (service.civil_servant_decided_at.present? && !civil_servant_agreed) =>
           I18n.t('service_states.civil_servant_rejected')
       }.fetch(true, nil)
     end
-    # rubocop:enable Metrics/AbcSize
   end
 end
