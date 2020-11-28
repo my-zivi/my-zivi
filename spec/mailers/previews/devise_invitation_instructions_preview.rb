cf1c6f800627d03b6f2c3dacad1f6ff9cc26aa80
Philipp Fehr <philipp@thefehr.me>
2020-11-28 19:36:04 +0100

    Update spec/mailers/previews/devise_invitation_instructions_preview.rb

--- spec/mailers/previews/devise_invitation_instructions_preview.rb
+++ spec/mailers/previews/devise_invitation_instructions_preview.rb
@@ -2,7 +2,7 @@
 
 # Preview all emails at http://localhost:3000/rails/mailers/devise_invitation_instructions/invitation_instructions
 class DeviseInvitationInstructionsPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/mailing_list_entry_notifier/notify
+  # Preview this email at http://localhost:3000/rails/mailers/devise_invitation_instructions/invitation_instructions
   def invitation_instructions
     Devise.mailer.invitation_instructions(CivilServant.last.user, 'fake-super-invitation-token')
   end
