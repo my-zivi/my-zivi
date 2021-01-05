98b84fa4af5c597d127f149379e468134c52f3d0
Philipp Fehr <philipp.fehr@myzivi.ch>
2021-01-05 19:51:23 +0100

    Extend spec

--- spec/requests/organizations/phone_list_controller_spec.rb
+++ spec/requests/organizations/phone_list_controller_spec.rb
@@ -13,9 +13,18 @@ RSpec.describe Organizations::PhoneListController, type: :request do
 
     let(:first_civil_servant) { create :civil_servant, :full, first_name: 'Hanspeter', last_name: 'Hugentobler' }
     let(:second_civil_servant) { create :civil_servant, :full }
-    let(:third_civil_servant) { create :civil_servant, :with_service,
-                                       service_attributes: [ :civil_servant_agreement_pending ], first_name: 'Not',
-                                       last_name: 'Here' }
+    let(:third_civil_servant) do
+      create :civil_servant, :with_service, :with_user,
+             first_name: 'Not', last_name: 'Here',
+             service_attributes: {
+               beginning: '2020-08-03',
+               ending: '2020-09-11',
+               service_specification: first_service_specification
+             },
+             service_traits: [
+               :civil_servant_agreement_pending
+             ]
+    end
 
     let(:first_service) do
       create :service,
@@ -64,6 +73,11 @@ RSpec.describe Organizations::PhoneListController, type: :request do
             expect(response.body.scan(/#{second_service_specification.name}/).size).to eq 1
             expect(response.body.scan(/#{first_civil_servant.full_name}/).size).to eq 1
             expect(response.body.scan(/#{second_civil_servant.full_name}/).size).to eq 1
+          end
+
+          it 'does not return agreement services' do
+            expect(response).to have_http_status(:success)
+            expect(response.body.scan(/#{third_civil_servant.full_name}/).size).to eq 0
           end
         end
 
