d998d55ef18b844305f0e09d8ab15402257fae31
Lukas Bischof <lukas@myzivi.ch>
2021-03-20 13:53:51 +0100

    Navitem helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,10 @@ module ApplicationHelper
       concat image_pack_tag("#{basepath}.png", **kwargs)
     end
   end
+
+  def navbar_link(name, path)
+    tag.li(class: "nav-item #{current_page?(path) ? 'active' : ''}".squish) do
+      link_to(name, path, class: 'nav-link')
+    end
+  end
 end
