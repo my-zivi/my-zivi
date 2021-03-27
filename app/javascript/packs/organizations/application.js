36e572bfcf6a7fff9f9c3c3f3449bcc395dc3949
Lukas Bischof <lukas@myzivi.ch>
2021-03-27 22:41:31 +0100

    --wip-- [skip ci]

--- app/javascript/packs/organizations/application.js
+++ app/javascript/packs/organizations/application.js
@@ -1,4 +1,1 @@
-import '../application';
 import '../../js/init';
-import '../../stylesheets/organizations/organization_members.scss';
-import '../../stylesheets/organizations/services/embedded_app.scss';
