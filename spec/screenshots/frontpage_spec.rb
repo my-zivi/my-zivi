fc96abe411702ad78ffcd58052f43e1759529f97
Lukas Bischof <lukas@myzivi.ch>
2021-04-26 23:34:21 +0200

    Fix date

--- spec/screenshots/frontpage_spec.rb
+++ spec/screenshots/frontpage_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 require 'percy'
 
 RSpec.describe 'Frontpage screenshots', type: :system, js: true do
-  before { create_list(:job_posting, 4) }
+  before { create_list(:job_posting, 4, publication_date: Date.new(2020, 1, 1)) }
 
   it 'renders front page correctly' do
     visit root_path
