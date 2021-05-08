e5543a691569512cc03434c83332f7868a5b9f90
Lukas Bischof <lukas@myzivi.ch>
2021-05-08 13:56:43 +0200

    Make migration easier

--- db/migrate/20210508095658_add_slug_to_blog_entry.rb
+++ db/migrate/20210508095658_add_slug_to_blog_entry.rb
@@ -1,17 +1,16 @@
 class AddSlugToBlogEntry < ActiveRecord::Migration[6.1]
-  def change
+  def up
     add_column :blog_entries, :slug, :string
 
-    reversible do |dir|
-      dir.up do
-        execute <<~SQL.squish.freeze
-          UPDATE blog_entries SET slug = LOWER(REPLACE(title, ' ', '-'))
-        SQL
-      end
-      dir.down {}
-    end
+    execute <<~SQL.squish.freeze
+      UPDATE blog_entries SET slug = LOWER(REPLACE(title, ' ', '-'))
+    SQL
 
     add_index :blog_entries, :slug, unique: true
     change_column_null :blog_entries, :slug, false
+  end
+
+  def down
+    remove_column :blog_entries, :slug
   end
 end
