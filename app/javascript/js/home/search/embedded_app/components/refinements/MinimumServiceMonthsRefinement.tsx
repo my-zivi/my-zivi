5604b15d296770eefcadf523ab6d726f9c9b46ee
Lukas Bischof <lukas@myzivi.ch>
2021-06-29 14:16:55 +0200

    Format Files

--- app/javascript/js/home/search/embedded_app/components/refinements/MinimumServiceMonthsRefinement.tsx
+++ app/javascript/js/home/search/embedded_app/components/refinements/MinimumServiceMonthsRefinement.tsx
@@ -3,14 +3,12 @@ import React from 'preact/compat';
 import { orderBy } from 'lodash';
 import defaultTranslations from './defaultTranslations';
 
-const transformItems = (items) => {
-  return orderBy(items, 'label', 'asc')
-    .map((item) => {
-      const { one, other } = MyZivi.translations.search.refinements.month;
-      const translation = item.label === '1' ? one : other;
-      return ({ ...item, label: `${item.label} ${translation}` });
-    });
-};
+const transformItems = (items) => orderBy(items, 'label', 'asc')
+  .map((item) => {
+    const { one, other } = MyZivi.translations.search.refinements.month;
+    const translation = item.label === '1' ? one : other;
+    return ({ ...item, label: `${item.label} ${translation}` });
+  });
 
 const MinimumServiceMonthsRefinement: React.FunctionComponent = () => (
   <RefinementList
