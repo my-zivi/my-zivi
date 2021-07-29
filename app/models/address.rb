150f62250c5e3ac7e1d8d6fc763810ee1bd8d536
Philipp Fehr <philipp@thefehr.me>
2021-07-29 08:55:19 +0200

    Add dependent to has_one

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Address < ApplicationRecord
-  has_one :organization
+  has_one :organization, dependent: :restrict_with_exception
 
   validates :city, :zip, :street, :primary_line, presence: true
   validates :zip, numericality: { only_integer: true, greater_than_or_equal_to: 1000, less_than: 10_000 }
