edf3d73385387674fd4914eb43068c6f496066cb
Lukas Bischof <lukas@myzivi.ch>
2021-09-17 00:26:19 +0200

    Update job spec

--- spec/jobs/job_posting_sync_job_spec.rb
+++ spec/jobs/job_posting_sync_job_spec.rb
@@ -4,17 +4,22 @@ require 'rails_helper'
 
 RSpec.describe JobPostingSyncJob, type: :job do
   describe '#perform' do
-    let(:poller) { instance_double('JobPostingApi::Poller', perform: [build(:job_posting)]) }
+    let(:deactivated_postings_poller) { instance_double('JobPostingApi::DeactivatedJobPostingsPoller', perform: true) }
+    let(:current_postings_poller) do
+      instance_double('JobPostingApi::CurrentJobPostingsPoller', perform: [build(:job_posting)])
+    end
 
     before do
-      allow(JobPostingApi::CurrentJobPostingsPoller).to receive(:new).and_return poller
+      allow(JobPostingApi::CurrentJobPostingsPoller).to receive(:new).and_return current_postings_poller
+      allow(JobPostingApi::DeactivatedJobPostingsPoller).to receive(:new).and_return deactivated_postings_poller
       allow(JobPosting).to receive(:reindex!)
     end
 
-    it 'calls polling service' do
+    it 'calls sync and deactivated service' do
       described_class.new.perform
       expect(JobPosting).to have_received(:reindex!)
-      expect(poller).to have_received(:perform)
+      expect(current_postings_poller).to have_received(:perform)
+      expect(deactivated_postings_poller).to have_received(:perform)
     end
   end
 end
