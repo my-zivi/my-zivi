2edfcfbf1387da4b0ae1196ebeb8bb380f6d046d
Lukas Bischof <lukas@myzivi.ch>
2021-10-21 21:00:44 +0200

    Fix map

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,7 @@ const { environment } = require('@rails/webpacker');
 const webpack = require('webpack');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 const typescript = require('./loaders/typescript');
+const path = require('path');
 
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
@@ -13,12 +14,18 @@ environment.plugins.append('MomentLocalesPlugin', new MomentLocalesPlugin({
   localesToKeep: ['de', 'fr', 'it'],
 }));
 
+const leafletPathSegments = '../../node_modules/leaflet/dist/images';
 environment.config.merge({
   resolve: {
     alias: {
       react: 'preact/compat',
       'react-dom/test-utils': 'preact/test-utils',
       'react-dom': 'preact/compat',
+      './images/layers.png$': path.resolve(__dirname, `${leafletPathSegments}/layers.png`),
+      './images/layers-2x.png$': path.resolve(__dirname, `${leafletPathSegments}/layers-2x.png`),
+      './images/marker-icon.png$': path.resolve(__dirname, `${leafletPathSegments}/marker-icon.png`),
+      './images/marker-icon-2x.png$': path.resolve(__dirname, `${leafletPathSegments}/marker-icon-2x.png`),
+      './images/marker-shadow.png$': path.resolve(__dirname, `${leafletPathSegments}/marker-shadow.png`),
     },
   },
 });
