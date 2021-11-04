7df6c6e07dc94689e2dc32101841854d40e44c65
Philipp Fehr <philipp.fehr@myzivi.ch>
2021-11-04 19:46:48 +0100

    Add breadcrumbs spec

--- spec/requests/organizations/organization_members_controller_spec.rb
+++ spec/requests/organizations/organization_members_controller_spec.rb
@@ -13,6 +13,9 @@ RSpec.describe Organizations::OrganizationMembersController, type: :request do
       subject { response }
 
       let(:outside_organization) { create :organization, :with_admin }
+      let(:expected_breadcrumbs) do
+        [I18n.t('loaf.breadcrumbs.organizations.organization_members.index')]
+      end
       let(:perform_request) { get organizations_members_path }
       let!(:outside_organization_member) do
         create(:organization_member,
@@ -27,6 +30,10 @@ RSpec.describe Organizations::OrganizationMembersController, type: :request do
         perform_request
       end
 
+      it_behaves_like 'validates presence of breadcrumbs' do
+        let(:breadcrumbs) { expected_breadcrumbs }
+      end
+
       it 'returns a list of organization members inside the organization' do
         expect(response).to be_successful
         expect(response.body).to include(*organization.organization_members.pick(:first_name, :last_name))
@@ -39,6 +46,17 @@ RSpec.describe Organizations::OrganizationMembersController, type: :request do
 
     describe '#new' do
       before { get new_organizations_member_path }
+
+      let(:expected_breadcrumbs) do
+        [
+          I18n.t('loaf.breadcrumbs.organizations.organization_members.index'),
+          I18n.t('loaf.breadcrumbs.organizations.organization_members.new')
+        ]
+      end
+
+      it_behaves_like 'validates presence of breadcrumbs' do
+        let(:breadcrumbs) { expected_breadcrumbs }
+      end
 
       it 'returns a successful response' do
         expect(response).to have_http_status(:ok)
@@ -73,9 +91,16 @@ RSpec.describe Organizations::OrganizationMembersController, type: :request do
 
     describe '#edit' do
       let(:editing_organization_member) { organization_admin }
+      let(:expected_breadcrumbs) do
+        [I18n.t('loaf.breadcrumbs.organizations.organization_members.profile')]
+      end
       let(:perform_request) { get edit_organizations_member_path(editing_organization_member) }
 
       before { perform_request }
+
+      it_behaves_like 'validates presence of breadcrumbs' do
+        let(:breadcrumbs) { expected_breadcrumbs }
+      end
 
       it 'renders the form' do
         expect(response).to be_successful
@@ -86,10 +111,20 @@ RSpec.describe Organizations::OrganizationMembersController, type: :request do
       context 'when the editing organization member is not the edited one' do
         let(:perform_request) { get edit_organizations_member_path(edited_organization_member) }
         let(:edited_organization_member) { create :organization_member, organization: organization }
-        
+
+        let(:expected_breadcrumbs) do
+          [
+            I18n.t('loaf.breadcrumbs.organizations.organization_members.index'),
+            edited_organization_member.full_name
+          ]
+        end
+
+        it_behaves_like 'validates presence of breadcrumbs' do
+          let(:breadcrumbs) { expected_breadcrumbs }
+        end
+
         it 'renders the form' do
           expect(response).to be_successful
-          expect(response.body).to include I18n.t('loaf.breadcrumbs.organizations.organization_members.index')
           expect(response).to render_template 'organizations/organization_members/edit'
         end
       end
---
+++ spec/support/organizations/breadcrumb_spec_helper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples_for 'validates presence of breadcrumbs' do
+  let(:html) { Nokogiri::XML(response.body, &:noblanks) }
+
+  it 'validates that all breadcrumbs are present' do
+    last_breadcrumb_index = breadcrumbs.length
+    last_breadcrumb = breadcrumbs.pop
+
+    breadcrumbs.each_with_index do |breadcrumb, index|
+      index += 1
+      expect(html.at_css(".breadcrumb .breadcrumb-item:nth-child(#{index}) a").text).to eq breadcrumb
+    end
+
+    expect(html.at_css(".breadcrumb .breadcrumb-item:nth-child(#{last_breadcrumb_index})").text).to eq last_breadcrumb
+  end
+end
