7a26bb577efe8ea8afb2c32c768b1976785d80f6
Philipp Fehr <philipp.fehr@myzivi.ch>
2021-11-21 18:30:21 +0100

    Improve structured faq

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@
 class HomeController < ApplicationController
   before_action -> { I18n.locale = :'de-CH' }
 
+  before_action :load_faqs, only: :civil_servant_faq
+  before_action :load_structured_faqs, only: :civil_servant_faq
+
   def index; end
 
   def for_organizations; end
@@ -13,13 +16,31 @@ class HomeController < ApplicationController
 
   def recruiting; end
 
-  def civil_servant_faq
-    @faqs = Faq.all
-  end
+  def civil_servant_faq; end
 
   def agb; end
 
   def privacy_policy; end
 
   def about_us; end
+
+  private
+
+  def load_faqs
+    @faqs = Faq.all
+  end
+
+  def load_structured_faqs
+    @faqs_structured = {
+      '@context': 'https://schema.org',
+      '@type': 'FAQPage',
+      mainEntity: Faq.all.map do |faq|
+        {
+          '@type': 'Question',
+          name: faq.question,
+          acceptedAnswer: { '@type': 'Answer', text: faq.answer }
+        }
+      end
+    }
+  end
 end
