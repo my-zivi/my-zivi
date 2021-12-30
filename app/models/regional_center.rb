498d63476ba21218fe23872c5bf06f5a32a71ed0
Lukas Bischof <lukas@myzivi.ch>
2021-12-30 23:20:02 +0100

    Update Links Configuration (#413)
    * Migrate urls to config
    
    * Migrate regional centers to file
    
    * Actually update URL
    
    * Fix spec

--- app/models/regional_center.rb
+++ app/models/regional_center.rb
@@ -2,14 +2,16 @@
 
 class RegionalCenter
   include ActiveModel::Model
-
-  validates :name, :address, presence: true
+  include ActiveModel::Attributes
 
   attr_accessor :name, :address, :identifier
 
+  validates :name, :address, presence: true
+  attribute :name, :string
+
   class << self
     Rails.configuration.x.regional_centers.each do |identifier, config|
-      downcased_identifier = identifier.downcase
+      downcased_identifier = identifier.to_s.downcase
 
       define_method ActiveSupport::Inflector.transliterate(downcased_identifier, locale: :'de-CH') do
         RegionalCenter.new(config.merge(
@@ -20,8 +22,8 @@ class RegionalCenter
     end
 
     def find(identifier)
-      id = identifier.downcase
-      return nil unless Rails.configuration.x.regional_centers.key? id
+      id = identifier.to_s.downcase
+      return nil unless Rails.configuration.x.regional_centers.key? id.to_sym
 
       public_send(ActiveSupport::Inflector.transliterate(id))
     end
