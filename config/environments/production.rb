0fe9acb20c854e7ce11a879ef338453a159ae538
Lukas Bischof <lukas@myzivi.ch>
2022-03-06 22:28:41 +0100

    Disable assets compilation in production (#452)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  config.assets.compile = false
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
