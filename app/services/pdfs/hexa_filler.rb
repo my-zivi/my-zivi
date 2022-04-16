f58294e3615ceabd56c689b0bcf7c74bada41cd9
Philipp Fehr <philipp@thefehr.me>
2022-04-16 17:31:07 +0200

    Replace PDFFormfiller with Hexapdf implementation (#471)
    * WIP: Replace PDFFormfiller with Hexapdf implementation
    
    * Add newest version of pdfs and change filler definition to fit
    
    * Lint
    
    * Delete unecessary java relicts

---
+++ app/services/pdfs/hexa_filler.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Pdfs
+  class HexaFiller
+    def initialize(file_path)
+      @file_path = file_path
+      @doc = HexaPDF::Document.open(@file_path)
+    end
+
+    def fill_field(name, data)
+      form = @doc.acro_form
+      field = form.field_by_name(name)
+      field.field_value = data.to_s
+    end
+
+    def save_as(file_path)
+      @doc.write(file_path, validate: false, incremental: false)
+    end
+  end
+end
