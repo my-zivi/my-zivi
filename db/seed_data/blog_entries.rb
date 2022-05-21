446091d9f2cbb287a72d733f992d1455f4c591a0
Lukas Bischof <lukas@myzivi.ch>
2022-05-21 22:30:16 +0200

    Extend seeds

--- db/seed_data/blog_entries.rb
+++ db/seed_data/blog_entries.rb
@@ -16,4 +16,21 @@ BlogEntry.create!(
   TEXT
 )
 
+BlogEntry.create!(
+  title: 'How I became a developer',
+  subtitle: 'About a programmer journey',
+  description: 'Why are programmers so popular?',
+  author: 'Anon Ymous',
+  published: true,
+  tags: %w[article news],
+  content: <<~TEXT.squish
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel dolor nec leo mattis viverra eu et tellus.
+    Cras faucibus, augue vel dictum tempor, leo metus facilisis risus, ut consectetur diam erat id leo. Suspendisse
+    tincidunt libero sit amet magna sodales sodales. Cras convallis magna a arcu mollis finibus. Proin tortor lacus,
+    viverra nec tempus non, blandit vitae risus. Sed luctus eros a ipsum dapibus, id dictum tellus sollicitudin. Quisque
+    interdum laoreet tellus blandit iaculis. Donec consequat tellus vitae vulputate elementum.
+TEXT
+)
+
+
 puts '> Blog entries seeded'
