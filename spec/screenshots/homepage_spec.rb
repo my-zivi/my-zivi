90b6f7e844240ffef6df868aed1247f915b5c381
Lukas Bischof <lukas@myzivi.ch>
2022-07-27 22:06:45 +0200

    Screenshot spec

--- spec/screenshots/homepage_spec.rb
+++ spec/screenshots/homepage_spec.rb
@@ -2,11 +2,16 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Frontpage screenshots', type: :system, js: true do
+RSpec.describe 'Homepage screenshots', type: :system, js: true do
   before { create_list(:job_posting, 4, publication_date: Date.new(2020, 1, 1)) }
 
   it 'renders front page correctly' do
     visit root_path
     page.percy_snapshot(page, { name: 'Frontpage' })
+  end
+
+  it 'renders pricing page correctly' do
+    visit pricing_path
+    page.percy_snapshot(page, { name: 'Pricing' })
   end
 end
