a3a54df13f0f8679e128897279fe5fc02738b636
Lukas Bischof <lukas@myzivi.ch>
2022-08-03 19:19:39 +0200

    Add 1 test

--- spec/helpers/navbar_helper_spec.rb
+++ spec/helpers/navbar_helper_spec.rb
@@ -20,4 +20,23 @@ RSpec.describe NavbarHelper do
       end
     end
   end
+
+  describe '#dropdown_navbar_link' do
+    subject(:dropdown) { helper.dropdown_navbar_link('MyDropdown', '/my-path', *dropdown_items) }
+
+    let(:dropdown_items) { [helper.dropdown_item('MyItem', '/my-item-path')] }
+
+    it 'creates a dropdown link' do
+      expect(dropdown).to eq(<<~HTML.squish.gsub(/> </, '><'))
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-expanded="false" href="/my-path">MyDropdown</a>
+          <div class="dropdown-menu dropdown-menu-card dropdown-caret mt-0">
+            <div class="bg-white py-2">
+              <a class="dropdown-item " href="/my-item-path">MyItem</a>
+            </div>
+          </div>
+        </li>
+      HTML
+    end
+  end
 end
