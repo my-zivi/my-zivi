4620ec0c9571ee65f9514326387b34f1dfc803ad
Lukas Bischof <lukas@myzivi.ch>
2022-11-24 21:36:54 +0100

    Add new job posting flags to edit form (#542)

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -480,6 +480,28 @@ SimpleForm.setup do |config|
     b.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
   end
 
+  config.wrappers :input_group_without_valid_classes,
+                  tag: 'div',
+                  class: 'form-group',
+                  error_class: 'form-group-invalid',
+                  valid_class: '' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :minlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+    b.use :label
+    b.wrapper :input_group_tag, tag: 'div', class: 'input-group' do |ba|
+      ba.optional :prepend
+      ba.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: ''
+      ba.optional :append
+    end
+    b.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback d-block' }
+    b.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
+  end
+
   # Floating Labels form
   #
   # floating labels default_wrapper
